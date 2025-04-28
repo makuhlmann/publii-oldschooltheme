efb301f25d7076d6b9be975d61dbc7b89bb9b91b
makuhlmann <19255462+makuhlmann@users.noreply.github.com>
2025-04-28 21:28:49 +0200

    Adapted for older browsers, for now with less customization

---
+++ OldschoolTheme/theme-variables.js
@@ -0,0 +1,11 @@
+/*
+ * Custom function used to generate the output of the theme variables; more about it can be found at https://getpublii.com/dev/theme-variables/
+ */
+
+var generateThemeVariables = function (params) {
+    let output = '';
+
+    return output;
+}
+
+module.exports = generateThemeVariables;
